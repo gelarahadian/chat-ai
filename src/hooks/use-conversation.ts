4583bf10a26827c717c3e03ee4b86c7fe4512ca8
gelarahadian <gelarahadian38@gmail.com>
2025-12-05 20:46:01 +0700

    Add: Scroll at the end'

--- src/hooks/use-conversation.ts
+++ src/hooks/use-conversation.ts
@@ -9,6 +9,15 @@ export const useGetConversations = () => {
 
 export const useGetConversationById = (id: string) => {
     return useMutation({
-        mutationFn: () => getConversationById(id)
-    })
+      mutationFn: () => getConversationById(id),
+      onSuccess: () => {
+        console.log(document.body.scrollHeight);
+        setTimeout(() => {
+          window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "auto",
+          });
+        }, 10);
+      },
+    });
 }
