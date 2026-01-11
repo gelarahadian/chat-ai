1f1aaabad0fb73f2ce88ae3cebb1c121b3e45a6e
gelarahadian <gelar.rahadian.fajar@gmail.com>
2026-01-11 19:01:06 +0700

    Fix: get baseurl from .env in chat context

--- src/contexts/chat-context.tsx
+++ src/contexts/chat-context.tsx
@@ -57,7 +57,7 @@ export const ChatProvider = ({ children }: { children: React.ReactNode }) => {
 
     bufferRef.current = "";
 
-    const res = await fetch("http://localhost:8080/api/chat/stream", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat/stream`, {
       method: "POST",
       body: JSON.stringify({ input, conversationId, chatIds }),
       headers: {
